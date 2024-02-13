3107c5faae3a2a7d2f00d63e0e91029a7ee998ad
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2024-02-13 19:38:45 +0100

    makes landing page mobile responsive

---
+++ public/assets/controllers/general_controller-33821bdd2868226d97d81dc2e8d062d4489837d8d38fb90402da7f9ad305a6cb.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="general"
+export default class extends Controller {
+  connect() {
+  }
+
+  menubar_toggle(event){
+    event.preventDefault()
+    const openMenuBar = document.getElementById('open-menu-toggle')
+    const closedMenuBar = document.getElementById('closed-menu-toggle')
+    const loginButton = document.getElementById('login-button')
+    const menuCard = document.getElementById('menu-card')
+    // loginButton.classList.toggle('log-in-button')
+    menuCard.classList.toggle('hidden')
+    openMenuBar.classList.toggle('hidden')
+    closedMenuBar.classList.toggle('hidden')
+
+  }
+
+  menuItemsClose(event){
+    event.preventDefault()
+    const openMenuBar = document.getElementById('open-menu-toggle')
+    const closedMenuBar = document.getElementById('closed-menu-toggle')
+    const loginButton = document.getElementById('login-button')
+    const menuCard = document.getElementById('menu-card')
+    // loginButton.classList.toggle('log-in-button')
+    menuCard.style.display = "hidden"
+    openMenuBar.classList.toggle('hidden')
+    closedMenuBar.classList.toggle('hidden')
+
+  }
+};
