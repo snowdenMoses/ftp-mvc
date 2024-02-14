9b8eee22e89198a041b335d327f858303a452390
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2024-02-14 01:54:40 +0100

    refactors consumers dashboard panel

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
             height: {
                 'category': '100px',
             },
+            screens: {
+                'sm': '640px',   // Small screens
+                'md': '768px',   // Medium screens
+                'lg': '1024px',  // Large screens
+                'xl': '1280px',  // Extra large screens
+            },
             fontFamily: {
                 poppins: ["Poppins", "sans-serif"],
                 'body': [
--- public/assets/controllers/general_controller-63bfbb17cd5a7c136438a21922169cad06063a86ffe95eca9875762a58b4584a.js
+++ public/assets/controllers/general_controller-63bfbb17cd5a7c136438a21922169cad06063a86ffe95eca9875762a58b4584a.js
@@ -44,4 +44,4 @@ export default class extends Controller {
     sidePanelClosedMenuBar.classList.toggle("hidden")
 
   }
-}
+};
