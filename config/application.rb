4fac4a4e4ce014569d64034b99b35814da9664f1
AbubakarMoses <111445710+AbubakarMoses@users.noreply.github.com>
2024-02-19 17:21:27 +0100

    adds and revokes roles and permissions, adds products and category to admin page

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module FtpMvc
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    # config.autoloader = :classic
+    config.autoloader = :classic
     # config.assets.precompile += %w( application.css )
 
     # Configuration for the application, engines, and railties goes here.
